f59944a605fb5f4a98ce7f7fb7976e2bd6e75689
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-11-29 22:49:55 +0200

    gruvbox

--- config.el
+++ config.el
@@ -35,7 +35,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-gruvbox)
+(setq doom-gruvbox-dark-variant "soft") ;; this is equivalent to the normal gruvbox dark
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -44,7 +45,6 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.

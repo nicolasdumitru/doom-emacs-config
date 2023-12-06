274fc4143456bb1093be8b9b94a8535cc707c768
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-12-06 23:34:45 +0200

    fixed gruvbox

--- config.el
+++ config.el
@@ -36,7 +36,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
-(setq doom-gruvbox-dark-variant "soft") ;; this is equivalent to the normal gruvbox dark
+(setq doom-gruvbox-dark-variant nil) ;; this is equivalent to the normal gruvbox dark
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
